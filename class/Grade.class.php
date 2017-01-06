8aaa0d78ad2f1163fde89e78ca1bb01cc2210ea1
AUGST Maxime <maxime.augst@iut-valence.fr>
2017-01-06 09:34:21 +0100

    Grade correctif + ajout risque

--- class/Grade.class.php
+++ class/Grade.class.php
@@ -1,6 +1,12 @@
 <?php
 class Grade{
     static function get(&$societe){
+        
+        $risque = $societe->array_options["options_risque"];
+        if ($risque < 60){
+                $point++;
+        }
+        
         $dpt = substr($societe->zip,0,2);
         $point = 0;
         if ($dpt == '26' || $dpt == '07'){
@@ -12,10 +18,15 @@ class Grade{
         if($societe->capital>5000) $point++;
         if($societe->capital>50000) $point++;
         if($societe->capital>500000) $point++;
+        if($risque == 100) $point = 0;
+        
         $grades = array("F", "E", "D", "C","B","A");
-        return $grade[$point];
+        return $grades[$point];
     
     }
 }
 
 ?>
+
+
+
--- script/create-maj-base.php
+++ script/create-maj-base.php
@@ -18,3 +18,8 @@ $PDOdb=new TPDOdb;
 $o=new TSimple208000($db);
 $o->init_db_by_vars($PDOdb);
 
+global $db
+
+dol_include_once('/core/class/extrafields.class.php');
+$extrafields = new ExtraFields($db);
+$res = $extrafields->addExtrField('risque', 'Risque', 'double', 0 , '10,2', 'societe');
